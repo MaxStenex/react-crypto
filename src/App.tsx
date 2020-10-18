62814dc0f78ba133e2218376b1a733402a544301
Maxim <yourstenex@gmail.com>
2020-10-18 19:36:29 +0300

    created basic html template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import './normalize.scss';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-wrapper'>
+      <div className='app-wrapper'>
+        <main>
+          <section className='crypto-tracking'>
+            <div className='container'>
+              <div className='crypto-tracking__wrapper'>
+                <h2 className='crypto-tracking__title'>Cryptocurrency rate</h2>
+                <div className='crypto-tracking__header crypto-tracking__row'>
+                  <div className='crypto-tracking__column crypto-tracking__currency-header'>
+                    Currency
+                  </div>
+                  <div className='crypto-tracking__column crypto-tracking__value-header'>
+                    Value
+                  </div>
+                  <div className='crypto-tracking__column crypto-tracking__rank-header'>
+                    Rank
+                  </div>
+                  <div className='crypto-tracking__column crypto-tracking__favorite-header'>
+                    Favorite
+                  </div>
+                </div>
+                <div className='crypto-tracking__row crypto-tracking__currency-main'>
+                  <div className='crypto-tracking__column crypto-tracking__currency-name'>
+                    <img
+                      src='https://ru.bitcoinwiki.org/upload/ru/images/5/55/Bitcoin_logo.jpg'
+                      alt=''
+                    />
+                    <span>Bitcoin</span>
+                  </div>
+                  <div className='crypto-tracking__column crypto-tracking__currency-price'>
+                    <span>100000$</span>
+                  </div>
+                  <div className='crypto-tracking__column crypto-tracking__currency-rank'>
+                    <span>1</span>
+                  </div>
+                  <div className='crypto-tracking__column crypto-tracking__currency-favorite'>
+                    <span>yep</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </main>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/api.ts
@@ -0,0 +1,7 @@
+type Api = {
+  key: string;
+};
+
+export const api: Api = {
+  key: 'd788e4ad9c24b88e5fc08551990c2eb3',
+};
